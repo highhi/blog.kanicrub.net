d93494f883f4e54625bd331b380a1f54fafad760
highhi <kazu8160@gmail.com>
2023-01-27 02:17:29 +0900

    記事を更新したらキャッシュをパージする

--- packages/worker/src/index.ts
+++ packages/worker/src/index.ts
@@ -10,14 +10,30 @@ interface Env {
 const app = new Hono<{ Bindings: Env }>()
 app.use('*', logger())
 
-app.post('/purge', async (c) => {
+app.post('/webhooks/purge', async (c) => {
   const sig = c.req.header('x-microcms-signature')
-  const payload = await c.req.text()
-  const res = await verify({ sig, secret: c.env.CMS_WEBHOOK_SECRET, payload })
+  const payload = await c.req.json<{ id: string }>()
+  const res = await verify({ sig, secret: c.env.CMS_WEBHOOK_SECRET, payload: JSON.stringify(payload) })
   
   if (!res) {
     throw new Error('Invalid signature.')
   }
+
+  const resp = await fetch('https://api.cloudflare.com/client/v4/zones/b3777550a260912b4fe163cb0dd3b7e2/purge_cache', {
+    method: 'post',
+    headers: new Headers({
+      'Authorization': `Bearer ${c.env.CACHE_PURGE_TOKEN}`,
+      'Content-Type': 'application/json'
+    }),
+    body: JSON.stringify({
+      files: [`https://blog.kanicrub.net/posts${payload.id}`]
+    })
+  })
+
+  if (!resp.ok) {
+    console.log('Cache purge failed')
+  }
+
   return c.text('Done')
 })
 
