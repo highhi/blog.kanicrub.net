e8e65ace65247a20a3e2d11d8545fee58e6faf4f
highhi <kazu8160@gmail.com>
2023-01-27 23:21:34 +0900

    v2からこのハックが不要になった

--- packages/blog/astro.config.mjs
+++ packages/blog/astro.config.mjs
@@ -7,11 +7,4 @@ export default defineConfig({
   adapter: cloudflare(),
   site: 'https://blog.kanicrub.net',
   integrations: [prefetch()],
-  vite: {
-    define: {
-      // see: https://github.com/withastro/astro/issues/4416#issuecomment-1228234136
-      'process.env.CMS_SERVICE_DOMAIN': JSON.stringify(process.env.CMS_SERVICE_DOMAIN),
-      'process.env.CMS_API_KEY': JSON.stringify(process.env.CMS_API_KEY)
-    }
-  }
 });
