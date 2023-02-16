6eb6dde962d32e885b12bee5feef1779b6b88b9d
highhi <kazu8160@gmail.com>
2023-02-16 23:19:50 +0900

    base pathを設定

--- packages/blog/astro.config.mjs
+++ packages/blog/astro.config.mjs
@@ -7,14 +7,14 @@ export default defineConfig({
   adapter: cloudflare(),
   site: 'https://blog.kanicrub.net',
   integrations: [prefetch()],
-  build: {
-    assets: 'https://assets-blog.kanicrub.net/assets'
-  },
   vite: {
     define: {
       // see: https://github.com/withastro/astro/issues/4416#issuecomment-1228234136
       'process.env.CMS_SERVICE_DOMAIN': JSON.stringify(process.env.CMS_SERVICE_DOMAIN),
       'process.env.CMS_API_KEY': JSON.stringify(process.env.CMS_API_KEY)
     }
+  },
+  vite: {
+    base: 'https://assets-blog.kanicrub.net'
   }
 });
