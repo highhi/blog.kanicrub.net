912a5fa65efb96183e8b295d604c4c2ef1a65d68
highhi <kazu8160@gmail.com>
2023-02-16 23:39:41 +0900

    update astro

--- packages/blog/astro.config.mjs
+++ packages/blog/astro.config.mjs
@@ -8,13 +8,15 @@ export default defineConfig({
   site: 'https://blog.kanicrub.net',
   integrations: [prefetch()],
   vite: {
+    ssr: {
+      // see: https://docs.astro.build/ja/guides/integrations-guide/node/#syntaxerror-named-export-compile-not-found
+      noExternal: ["path-to-regexp"]
+    },
+    base: 'https://assets-blog.kanicrub.net',
     define: {
       // see: https://github.com/withastro/astro/issues/4416#issuecomment-1228234136
       'process.env.CMS_SERVICE_DOMAIN': JSON.stringify(process.env.CMS_SERVICE_DOMAIN),
       'process.env.CMS_API_KEY': JSON.stringify(process.env.CMS_API_KEY)
     }
   },
-  vite: {
-    base: 'https://assets-blog.kanicrub.net'
-  }
 });
