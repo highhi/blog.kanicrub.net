4dbd4117abb2c1e183036e0c7fb280048e30b5ed
highhi <kazu8160@gmail.com>
2023-04-08 21:18:28 +0900

    fix

--- packages/blog/astro.config.mjs
+++ packages/blog/astro.config.mjs
@@ -6,13 +6,15 @@ export default defineConfig({
   output: 'server',
   adapter: cloudflare(),
   site: 'https://blog.kanicrub.net',
+  build: {
+    assetsPrefix: import.meta.env.PROD ? 'https://assets-blog.kanicrub.net' : undefined
+  },
   integrations: [prefetch()],
   vite: {
     ssr: {
       // see: https://docs.astro.build/ja/guides/integrations-guide/node/#syntaxerror-named-export-compile-not-found
       noExternal: ["path-to-regexp"]
     },
-    base: 'https://assets-blog.kanicrub.net',
     define: {
       // see: https://github.com/withastro/astro/issues/4416#issuecomment-1228234136
       'process.env.CMS_SERVICE_DOMAIN': JSON.stringify(process.env.CMS_SERVICE_DOMAIN),
